a1c6cb605446f50585c1b421547c18cdcf3d067e
jai-turinton <jaikrishna.singh@turinton.com>
2025-10-04 00:32:18 +0530

    create REST API learning app

---
+++ rest-api/src/main/java/com/coding_shuttle/rest/rest_api/RestApiApplication.java
@@ -0,0 +1,18 @@
+package com.coding_shuttle.rest.rest_api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+	}
+}
